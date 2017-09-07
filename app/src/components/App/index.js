674e4ec8945eab5a31ff6020102cbebca7bf3e49
CoderDam <garnes.damien@gmail.com>
2017-09-07 12:34:28 +0200

    hello world

---
+++ app/src/components/App/index.js
@@ -0,0 +1,17 @@
+/* Npm import */
+import React from 'react';
+
+
+/* Local import */
+
+
+/* Code */
+const App = () => (
+  <main id="app">
+    Hello world!
+  </main>
+);
+
+
+/* Export */
+export default App;
---
+++ app/src/store/reducer.js
@@ -0,0 +1,30 @@
+/* Npm import */
+
+
+/* Local import */
+
+
+/* Code */
+
+
+/* initialState */
+const initialState = {};
+
+
+/* Types */
+
+
+/* Duck */
+const reducer = (state = initialState, action = {}) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+
+/* Action creators */
+
+
+/* Export */
+export default reducer;
